1e9b48f077e621ac63b36a5460ac547e823c7e42
in-sol-ence <sol.oberoi.m@gmail.com>
2025-07-18 00:56:15 -0500

    Edited EpislonGreedyConstant

--- EpsilonGreedyConst.py
+++ EpsilonGreedyConst.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import "k-arm-bandit"
+from kArmBandit import kbandit
+
 rng = np.random.default_rng()
 
 actions = 10
 ep = 0.1
 alpha = 0.2
-rewcum=0
-
+rewcum= 0
+stationary = False
 Q = np.zeros(actions)
 N = np.zeros(actions)
 steps = []
@@ -16,7 +17,7 @@ opt=[]
 opt.append(0)
 
 print(f'Starting stepsize constant method with ep = {ep} and stepsize const = {alpha}')
-bandit = kbandit(stat, actions)
+bandit = kbandit(stationary, actions)
 for step in range(10000):
     if rng.random() > ep:
         a = Q.argmax()
@@ -24,13 +25,5 @@ for step in range(10000):
         a = rng.integers(low=0, high=(actions-1))
     
     reward = bandit.kbandit(a)
-    rewcum+=reward
-    rewards.append(rewcum)
+
     Q[a] = Q[a] + alpha*(reward - Q[a])
-    steps.append(step)
-    if len(opt)>0 and step>0:
-        i=opt[len(opt)-1]
-        if a == bandit.optimal():
-            opt.append((i*(step-1) + 100)/step)
-        else:
-            opt.append(i*(step-1)/step)
