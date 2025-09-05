378c76923054a4952fa4b3333a4e4fa854005337
in-sol-ence <sol.oberoi.m@gmail.com>
2025-09-05 18:46:42 -0500

    Seperated into methods

--- jackCarDP.py
+++ jackCarDP.py
@@ -4,8 +4,6 @@ env = JackCarRental(10, 10, 1)
 
 print(env.get_state())
 
-
-
 ## Initialize policy and value function
 # Considering it is a deterministic environment, we can use a dictionary to store the policy and value function
 
@@ -20,39 +18,50 @@ for cars1 in range(21):
 theta = 0.1  # Learning rate
 delta = 0.0
 gamma = 0.9  # Discount factor
-policy_eval = 0
-oldpolicy = policy.copy() # This will be used in policy improvement later
 
-# Policy Evaluation
-while (delta < theta):
+def policyEval(value, policy, env, theta=0.1) -> dict:
+    """ Given state-values, a policy, certain environment dynamics and a value theta which defines accuracy of the state-values,
+    policyEval calculates new state-value pairs for the given policy to accuracy theta. 
+    
+    It returns the new state-value pairs"""
+    # Policy Evaluation
     delta = 0.0
+    policy_eval = 0
+    while (delta < theta):
+        delta = 0.0
+        for cars1 in range(21):
+            for cars2 in range(21):
+                v = value[(cars1, cars2)]
+                action = policy[(cars1, cars2)]
+                expectedRew = min(env.poisson_lambda_rent1, cars1) + min(env.poisson_lambda_rent2, cars2)
+                nextState = (max(0, cars1-action-env.poisson_lambda_rent1), max(0, cars2+action-env.poisson_lambda_rent2))
+                value[(cars1, cars2)] = expectedRew + gamma*value[nextState]
+                delta = max(delta, abs(v-value[(cars1, cars2)]))
+        policy_eval += 1
+        print(f"Policy evaluation {policy_eval} completed with delta: {delta}.")
+    
+    return value
+
+def policyImprov(value, env) -> None:
+    """Given accurate (to theta) state-value pairs and certain environment dynamics,
+    policyImprov finds the best policy pi."""
+    policy_stable = True
     for cars1 in range(21):
         for cars2 in range(21):
-            v = value[(cars1, cars2)]
-            action = policy[(cars1, cars2)]
-            expectedRew = min(env.poisson_lambda_rent1, cars1) + min(env.poisson_lambda_rent2, cars2)
-            nextState = (max(0, cars1-action-env.poisson_lambda_rent1), max(0, cars2+action-env.poisson_lambda_rent2))
-            value[(cars1, cars2)] = expectedRew + gamma*value[nextState]
-            delta = max(delta, abs(v-value[(cars1, cars2)]))
-    policy_eval += 1
-    print(f"Policy evaluation {policy_eval} completed with delta: {delta}.")
+            best_action = policy[(cars1, cars2)]
+            argMax = value[(cars1, cars2)]
+            for action in range(-5, 6):
+                expectedRew = min(env.poisson_lambda_rent1, cars1) + min(env.poisson_lambda_rent2, cars2)
+                nextState = (max(0, cars1-action-env.poisson_lambda_rent1), max(0, cars2+action-env.poisson_lambda_rent2))
+                arg = expectedRew + gamma*value[nextState]
+                if arg > argMax:
+                    argMax = arg
+                    best_action = action
+                    policy_stable=False
 
-# Policy Improvement
-policy_stable = True
-for cars1 in range(21):
-    for cars2 in range(21):
-        best_action = policy[(cars1, cars2)]
-        argMax = value[(cars1, cars2)]
-        for action in range(-5, 6):
-            expectedRew = min(env.poisson_lambda_rent1, cars1) + min(env.poisson_lambda_rent2, cars2)
-            nextState = (max(0, cars1-action-env.poisson_lambda_rent1), max(0, cars2+action-env.poisson_lambda_rent2))
-            arg = expectedRew + gamma*value[nextState]
-            if arg > argMax:
-                argMax = arg
-                best_action = action
-                policy_stable=False
+    if policy_stable:
+        print("Found optimal policy")
+    else:
+        print("Going back in")
 
-if policy_stable:
-    print("Found optimal policy")
-else:
-    print("Going back in")     
+
